75fa14f0b90aa1367c8a71197b7347f2a0e77959
shafaq shaikh <shafaqshaikh10@gmail.com>
2020-07-02 22:12:18 +0530

    Add files via upload

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -125,8 +125,7 @@ STATIC_ROOT=os.path.join(BASE_DIR, "staticfiles")
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
+    os.path.join(BASE_DIR, "static")
 ]
 
 MEDIA_ROOT=os.path.join(BASE_DIR, "static/images")
