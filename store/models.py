397b792ffe06380e125fe7bbf456f8b7a6e8d6cd
shafaqshaikh <shafaqshaikh10@gmail.com>
2020-07-02 13:09:31 +0530

    ecommerce website

---
+++ store/models.py
@@ -0,0 +1,79 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Customer(models.Model):
+	user=models.OneToOneField(User , on_delete=models.CASCADE , null=True , blank=True)
+	name=models.CharField(max_length=200)
+	email=models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.name
+
+
+class Product(models.Model):
+	name=models.CharField(max_length=200)
+	price=models.FloatField(max_length=200)
+	digital=models.BooleanField(default=True , null=True , blank=True)
+	image=models.ImageField(null=True)
+
+	def __str__(self):
+		return self.name
+
+class Order(models.Model):
+	customer=models.ForeignKey(Customer , on_delete=models.SET_NULL , blank=True , null=True)
+	date_ordered=models.DateTimeField(auto_now_add=True)
+	complete=models.BooleanField(default=True , null=True , blank=True)
+	transaction_id=models.CharField(max_length=200 , null=True)
+
+	def __str__(self):
+		return str(self.id)	
+
+
+	@property
+	def shipping(self):
+		shipping = False
+		orderitems = self.orderitem_set.all()
+		for i in orderitems:
+			if i.product.digital == False:
+				shipping = True
+		return shipping
+
+
+	@property
+	def get_cart_total(self):
+		orderitems=self.orderitem_set.all()
+		total=sum([item.get_total for item in orderitems])
+		return total
+	@property
+	def get_cart_items(self):
+		orderitems=self.orderitem_set.all()
+		total=sum([item.qauntity for item in orderitems])
+		return total
+
+class OrderItem(models.Model):
+	product=models.ForeignKey(Product , on_delete=models.SET_NULL , blank=True , null=True)
+	order=models.ForeignKey(Order , on_delete=models.SET_NULL , blank=True , null=True)
+	qauntity=models.IntegerField(default=0 , null=True , blank=True)
+	date_added=models.DateTimeField(auto_now_add=True)
+
+	@property
+	def get_total(self):
+		total=self.product.price*self.qauntity
+		return total
+
+
+class ShippingAddress(models.Model):
+	customer=models.ForeignKey(Customer , on_delete=models.SET_NULL , blank=True , null=True)
+	order=models.ForeignKey(Order , on_delete=models.SET_NULL , blank=True , null=True)
+	address=models.CharField(max_length=200)
+	city=models.CharField(max_length=200)
+	state=models.CharField(max_length=200)
+	zipcode=models.CharField(max_length=200)
+	date_added=models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.address
+
+
+
+
