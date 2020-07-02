02e4b44cb2101de896b5753181aebd0d541e38b3
shafaq shaikh <shafaqshaikh10@gmail.com>
2020-07-02 13:40:47 +0530

    Add files via upload

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,6 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 MEDIA_URL='/images/'
+
+
+STATIC_ROOT=os.path.join(BASE_DIR, "staticfiles")
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
